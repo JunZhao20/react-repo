00840f0eb893161ee5dcada23b4d861caf4e5089
Jun Zhao <junzhao1111@gmail.com>
2023-10-06 17:59:51 +0100

    Created counter with buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  // this allows react to render the new changes of current counter by using setCounter
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
-}
+};
 
 export default App;
