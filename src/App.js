d9e5991a7d175084084e316c53a54165472124fa
Jun Zhao <junzhao1111@gmail.com>
2023-10-08 09:42:17 +0100

    Fetched API json data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  // this allows react to render the new changes of current counter by using setCounter
-  const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-    </div>
-  );
+  // useEffect uses a callback func (() => {API})
+  let apiKey = useEffect(() => {
+    fetch("http://www.omdbapi.com/?apikey=783da78e&")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  return <h1>App</h1>;
 };
 
+// Allows compounets to be exported and used in other files such as index.js
 export default App;
