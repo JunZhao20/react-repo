84464a49ef05b740da3724e6c26413a7f1b8c5f7
Jun Zhao <junzhao1111@gmail.com>
2023-10-12 11:17:10 +0100

    Chnaged render app script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App.js";
 
 // This allows react to renders and updates the compounants and contents in the App.js file to a div with the id of root.
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
