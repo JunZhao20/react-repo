3b4ceb4f65f3f3740d4ff4f9b3703759c8f4cc16
Jun Zhao <junzhao1111@gmail.com>
2023-10-22 13:37:04 +0100

    Added a form to contact component

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -1,6 +1,25 @@
 import React from "react";
-// class AddContact extends React.Component(
+class AddContact extends React.Component {
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Input Name: " />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Input Email: " />
+          </div>
+          <div className="ui centered grid padded">
+            <button className="ui primary button"> Add contact</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
-// );
-
-// export default AddContact
+export default AddContact;
