40ef9d820c811e6e3beaa3ce49e6b77421bb5603
Jun <j.zhao1@newcastle.ac.uk>
2023-10-23 15:19:34 +0100

    Altered classnames

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -2,8 +2,8 @@ import React from "react";
 class AddContact extends React.Component {
   render() {
     return (
-      <div className="ui main">
-        <h2>Add Contact</h2>
+      <div className="form">
+        <h2 className="add">Add Contact</h2>
         <form className="ui form">
           <div className="field">
             <label>Name</label>
