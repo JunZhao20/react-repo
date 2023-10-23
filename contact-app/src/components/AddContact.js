0d4ec8dacf53262cd4e22692b840aa093d97fbbc
Jun <j.zhao1@newcastle.ac.uk>
2023-10-23 16:19:32 +0100

    added event listner for user inputs

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -1,5 +1,10 @@
 import React from "react";
 class AddContact extends React.Component {
+  // Sets the current state of the component
+  state = {
+    name: "",
+    email: "",
+  };
   render() {
     return (
       <div className="form">
@@ -7,11 +12,25 @@ class AddContact extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Name</label>
-            <input type="text" name="name" placeholder="Input Name: " />
+            <input
+              type="text"
+              name="name"
+              placeholder="Input Name: "
+              //Storing current value of state.name
+              value={this.state.name}
+              // Setting the state instance 'name' with the user input
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
           </div>
           <div className="field">
             <label>Email</label>
-            <input type="text" name="email" placeholder="Input Email: " />
+            <input
+              type="text"
+              name="email"
+              placeholder="Input Email: "
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
           </div>
           <div className="ui centered grid padded">
             <button className="ui primary button"> Add contact</button>
