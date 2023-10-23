12658d026876716f2113ddf854aace5595c9d12a
Jun <j.zhao1@newcastle.ac.uk>
2023-10-23 15:18:55 +0100

    Added test contacts and linked props to ContactList

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -5,11 +5,24 @@ import AddContact from "./components/AddContact.js";
 import ContactList from "./components/ContactList.js";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Jun",
+      email: "jun@gmail.com",
+    },
+    {
+      id: "1",
+      name: "Bob",
+      email: "Bob@gmail.com",
+    },
+  ];
   return (
     <div className="container">
       <Header />
       <AddContact />
-      {/* <ContactList /> */}
+      {/* This adds contacts as a props that can be used in the child class*/}
+      <ContactList contacts={contacts} />
     </div>
   );
 }
