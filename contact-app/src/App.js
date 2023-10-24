0131be206a80c1b06005480f090be7ce8cdc2794
Jun Zhao <junzhao1111@gmail.com>
2023-10-24 11:29:40 +0100

    Added handlder to set new contacts

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -7,10 +7,18 @@ import ContactList from "./components/ContactList.js";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    // '...contacts' is a copy of current state of the contact list
+    // 'contact' is a new item that gets added into the end of the list.
+    setContacts([...contacts, contact]);
+    console.log(contacts);
+  };
+
   return (
     <div className="container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       {/* This adds contacts as a props that can be used in the child class*/}
       <ContactList contacts={contacts} />
     </div>
